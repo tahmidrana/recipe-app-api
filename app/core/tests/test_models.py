dc201064098b9d1a8bcacf10961b17ac32bc8409
tahmidrana <tahmidrana@gmail.com>
2020-01-16 00:22:08 +0600

    add custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,3 +29,6 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser('su@abc.com', 'test123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
